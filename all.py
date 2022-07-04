ed1a2df6150a324172b77459afb2e0f16e4cca47
ZhangWenHua <11846719@qq.com>
2022-07-04 14:19:26 +0800

    2022070402commit

--- all.py
+++ all.py
@@ -3,5 +3,6 @@ import os
 import pytest
 if __name__ == '__main__':
     # pytest.main(["--html=./report/report0218.html"])
+    #执行入口
     pytest.main()
     #os.system("allure generate ./temp --clean -o ./report --clean" )
